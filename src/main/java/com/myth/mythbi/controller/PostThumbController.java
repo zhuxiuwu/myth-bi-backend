ed2197e6cb53262d5b7aee4a3f06bd3617f6ca0d
yixiaochuan <3183023740@qq.com>
2023-10-12 18:51:55 +0800

    first init

---
+++ src/main/java/com/myth/mythbi/controller/PostThumbController.java
@@ -0,0 +1,55 @@
+package com.myth.mythbi.controller;
+
+import com.myth.mythbi.common.BaseResponse;
+import com.myth.mythbi.common.ErrorCode;
+import com.myth.mythbi.common.ResultUtils;
+import com.myth.mythbi.exception.BusinessException;
+import com.myth.mythbi.model.dto.postthumb.PostThumbAddRequest;
+import com.myth.mythbi.model.entity.User;
+import com.myth.mythbi.service.PostThumbService;
+import com.myth.mythbi.service.UserService;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+                                         HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
---
+++ src/main/java/com/myth/mythbi/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.myth.mythbi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.myth.mythbi.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
